9203e7c1a9d320f7d53feff763ca1e05b1d4eebd
jharte5 <jonathan.harte@codeimmersives.com>
2019-12-02 14:19:39 -0500

    inital commit

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-const makeDino = function() {
+const makeDino = function(species, period, carnivore, extinct = false) {
+  const dino = {
+  species, 
+  period, 
+  carnivore,
+  extinct,
+  }
+  return dino
 }
 
-const makeSingular = function() {
+const makeSingular = function(dino) {
+  if (!dino.species.endsWith('us')) {
+    return dino
+  } else {
+    return dino = {
+      species: dino.species.slice(0,-2),
+      period: dino.period,
+      carnivore: dino.carnivore,
+      extinct: dino.extinct,
+    }
+      
+  }
 }
 
 const truncateSpecies = function() {
+  
 }
 
 const makeExtinct = function() {
